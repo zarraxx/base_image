1e205f8acc315c3315b76f16fb6fe1f0a01e2f85
xsp <xsp@xyan.net.cn>
2026-07-05 12:19:13 +0800

    打开测试

--- ubuntu/24.04/darling/test.sh
+++ ubuntu/24.04/darling/test.sh
@@ -4,12 +4,12 @@ set -e
 IMAGE_NAME="${1:-darling:noble-20260608}"
 DOCKER_CMD="${DOCKER_CMD:-docker}"
 
-if [[ "${GITHUB_ACTIONS:-}" == "true" ]]; then
-  $DOCKER_CMD run --rm \
-    "${IMAGE_NAME}" \
-    bash -lc 'set -e; command -v darling; dpkg-query -W | grep -i "^darling"; echo hello-from-darling-image'
-  exit 0
-fi
+# if [[ "${GITHUB_ACTIONS:-}" == "true" ]]; then
+#   $DOCKER_CMD run --rm \
+#     "${IMAGE_NAME}" \
+#     bash -lc 'set -e; command -v darling; dpkg-query -W | grep -i "^darling"; echo hello-from-darling-image'
+#   exit 0
+# fi
 
 $DOCKER_CMD  run --rm -it \
   --privileged \
