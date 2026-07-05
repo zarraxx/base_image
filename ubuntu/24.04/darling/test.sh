ced1fa2af9a01d46838d8abc8d5f6622c6974a55
xsp <xsp@xyan.net.cn>
2026-07-05 12:17:45 +0800

    调整了darling的测试脚本

--- ubuntu/24.04/darling/test.sh
+++ ubuntu/24.04/darling/test.sh
@@ -4,18 +4,24 @@ set -e
 IMAGE_NAME="${1:-darling:noble-20260608}"
 DOCKER_CMD="${DOCKER_CMD:-docker}"
 
-if [[ "${GITHUB_ACTIONS:-}" == "true" ]]; then
-  $DOCKER_CMD run --rm \
-    "${IMAGE_NAME}" \
-    bash -lc 'set -e; command -v darling; dpkg-query -W | grep -i "^darling"; echo hello-from-darling-image'
-  exit 0
-fi
+# if [[ "${GITHUB_ACTIONS:-}" == "true" ]]; then
+#   $DOCKER_CMD run --rm \
+#     "${IMAGE_NAME}" \
+#     bash -lc 'set -e; command -v darling; dpkg-query -W | grep -i "^darling"; echo hello-from-darling-image'
+#   exit 0
+# fi
 
-$DOCKER_CMD run --rm \
+
+
+$DOCKER_CMD  run --rm -it \
   --privileged \
+  --userns=host \
   --security-opt seccomp=unconfined \
+  --security-opt label=disable \
+  --tmpfs /root:rw,exec,mode=755 \
+  --tmpfs /tmp:rw,exec,mode=1777 \
   -v "$PWD":/work \
-  "${IMAGE_NAME}" \
+  ghcr.io/zarraxx/darling:noble-20260608 \
   darling shell /bin/sh -lc 'uname -a; echo hello-from-darling'
 
 
